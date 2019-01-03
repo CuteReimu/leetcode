93c0a7ed1255350eb1d2ae3887cbf14dee20bc47
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-01-04 02:58:00 +0800

    51.n-queens

--- 51.n-queens/solution.cpp
+++ 51.n-queens/solution.cpp
@@ -4,61 +4,72 @@ public:
 		vector<vector<string>> result;
 		if (n > 0)
 		{
-			bool **curr = new bool *[n];
+			vector<int> nums;
 			for (int i = 0; i < n; i++)
+				nums.push_back(i);
+			vector<vector<int>> vec = permute(nums);
+			for (vector<vector<int>>::const_reference v : vec)
 			{
-				curr[i] = new bool[n];
-				memset(curr[i], false, n);
+				result.emplace_back();
+				for (vector<int>::const_reference i : v)
+				{
+					string s;
+					for (int j = 0; j < n; j++)
+						s += j == i ? 'Q' : '.';
+					result.back().push_back(move(s));
+				}
 			}
-			solveNQueens(result, curr, n, n, 0, 0);
-			for (int i = 0; i < n; i++)
-				delete[] curr[i];
-			delete[] curr;
 		}
 		return result;
 	}
-private:
-	void solveNQueens(vector<vector<string>> &result, bool **p, int max, int n, int curi, int curj)
-	{
-		int j = curj;
-		for (int i = curi; i < max; i++)
+
+	vector<vector<int>> permute(vector<int>& nums) {
+		list<vector<int>> result;
+		if (!nums.empty())
 		{
-			for (; j < max; j++)
+			result.push_back(vector<int>());
+			int count = 0;
+			for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
 			{
-				bool check = true;
-				for (int k = 0; k < max; k++)
+				int i = 0;
+				int iMax = result.size();
+				for (int i = 0; i < iMax; i++)
 				{
-					if (p[k][j] || p[i][k]
-						|| (j - i + k >= 0 && j - i + k < max && p[k][j - i + k])
-						|| (i + j - k >= 0 && i + j - k < max && p[k][i + j - k]))
+					list<vector<int>>::iterator itr = result.begin();
+					for (vector<int>::iterator iter = itr->begin(); iter != itr->end(); iter++)
 					{
-						check = false;
-						break;
+						vector<int> vec(itr->begin(), iter);
+						vec.push_back(*it);
+						vec.insert(vec.end(), iter, itr->end());
+						if (nums.size() - 1 != count || check(vec))
+								result.push_back(vec);
 					}
+					vector<int> vec(itr->begin(), itr->end());
+					vec.push_back(*it);
+					if (nums.size() - 1 != count || check(vec))
+							result.push_back(vec);
+					result.pop_front();
 				}
-				if (check)
-				{
-					p[i][j] = true;
-					if (n == 1)
-					{
-						result.emplace_back();
-						for (int x = 0; x < max; x++)
-						{
-							string s;
-							for (int y = 0; y < max; y++)
-								s += p[x][y] ? 'Q' : '.';
-							result.back().push_back(s);
-						}
-						p[i][j] = false;
-						return;
-					} else
-					{
-						solveNQueens(result, p, max, n - 1, i, j);
-						p[i][j] = false;
-					}
-				}
+				count++;
 			}
-			j = 0;
 		}
+		return vector<vector<int>>(result.begin(), result.end());
+	}
+
+	bool check(const vector<int> &vec)
+	{
+		set<int> s;
+		for (int i = 0; i < vec.size(); i++)
+		{
+			if (!s.insert(vec[i] - i).second)
+				return false;
+		}
+		s.clear();
+		for (int i = 0; i < vec.size(); i++)
+		{
+			if (!s.insert(vec[i] + i).second)
+				return false;
+		}
+		return true;
 	}
 };
