aa9079a8596d672f91a2c1c4e1b69f004fc30075
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-01-05 00:58:00 +0800

    52.n-queens-ii

---
+++ 52.n-queens-ii/solution.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+	int totalNQueens(int n) {
+		int result = 0;
+		int *answer = new int[n];
+		int **p = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			p[i] = new int[n];
+			memset(p[i], 0, n * sizeof(int));
+		}
+		solveNQueens(result, answer, p, n, 0);
+		for (int i = 0; i < n; i++)
+			delete[] p[i];
+		delete[] p;
+		return result;
+	}
+private:
+	void solveNQueens(int &result, int *answer, int **p, int n, int j)
+	{
+		if (j == n)
+		{
+			result++;
+			return;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (!p[j][i])
+			{
+				answer[j] = i;
+				for (int k = j + 1; k < n; k++)
+				{
+					p[k][i]++;
+					int i1 = k - j + i;
+					int i2 = j - k + i;
+					if (i1 < n)
+						p[k][i1]++;
+					if (i2 >= 0)
+						p[k][i2]++;
+				}
+				solveNQueens(result, answer, p, n, j + 1);
+				for (int k = j + 1; k < n; k++)
+				{
+					p[k][i]--;
+					int i1 = k - j + i;
+					int i2 = j - k + i;
+					if (i1 < n)
+						p[k][i1]--;
+					if (i2 >= 0)
+						p[k][i2]--;
+				}
+			}
+		}
+	}
+};
