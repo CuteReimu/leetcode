03b8a56d5970571e2753c7295f4f4e84a8141bfd
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-01-19 05:26:00 +0800

    1.two-sum

--- 1.two-sum/solution.java
+++ 1.two-sum/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++)
+            cache.put(nums[i], i);
         for (int i = 0; i < nums.length; i++)
         {
-            for (int j = i + 1; j < nums.length; j++)
+            int a = target - nums[i];
+            if (cache.containsKey(a))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    int[] result = {i, j};
+                int[] result = {i, cache.get(a)};
+                if (result[0] != result[1])
                     return result;
-                }
             }
         }
         int[] result = {};
