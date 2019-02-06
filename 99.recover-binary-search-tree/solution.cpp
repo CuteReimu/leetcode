1f681ebd82c5f81b7ecf1a78a5288656c074b703
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-02-06 11:39:00 +0800

    99.recover-binary-search-tree

--- 99.recover-binary-search-tree/solution.cpp
+++ 99.recover-binary-search-tree/solution.cpp
@@ -12,59 +12,46 @@ class Solution {
 public:
 	void recoverTree(TreeNode* root) {
 		if (!root) return;
-		mainRoot = root;
-		recoverOneTree(root);
-	}
-private:
-	TreeNode *mainRoot;
-	bool recoverOneTree(TreeNode *root)
-	{
-		if (root->left && (recoverTreeNode(root->left, root) || recoverOneTree(root->left)))
-			return true;
-		if (root->right && (recoverTreeNode(root->right, root) || recoverOneTree(root->right)))
-			return true;
-		if (root->left && root->right && recoverTreeTree(root->left, root->right))
-			return true;
-		return false;
-	}
-	bool recoverTreeTree(TreeNode *root1, TreeNode *root2)
-	{
-		if (recoverTreeNode(root1, root2) || recoverTreeNode(root2, root1))
-			return true;
-		if (root1->left && root2->left && recoverTreeTree(root1->left, root2->left))
-			return true;
-		if (root1->left && root2->right && recoverTreeTree(root1->left, root2->right))
-			return true;
-		if (root1->right && root2->left && recoverTreeTree(root1->right, root2->left))
-			return true;
-		if (root1->right && root2->right && recoverTreeTree(root1->right, root2->right))
-			return true;
-		return false;
-	}
-	bool recoverTreeNode(TreeNode *root1, TreeNode *root2)
-	{
-		if (recoverNodeNode(root1, root2))
-			return true;
-		if (root1->left && recoverTreeNode(root1->left, root2))
-			return true;
-		if (root1->right && recoverTreeNode(root1->right, root2))
-			return true;
-		return false;
-	}
-	bool recoverNodeNode(TreeNode *root1, TreeNode *root2)
-	{
-		if (!root1 || !root2)
-			return false;
-		swap(root1->val, root2->val);
-		if (isValidBST(mainRoot, LLONG_MIN, LLONG_MAX))
-			return true;
-		swap(root1->val, root2->val);
-		return false;
-	}
-	bool isValidBST(TreeNode* root, long long bigThan, long long smallThan)
-	{
-		if (!root) return true;
-		if (root->val <= bigThan || root->val >= smallThan) return false;
-		return isValidBST(root->left, bigThan, root->val) && isValidBST(root->right, root->val, smallThan);
+		stack<pair<TreeNode *, unsigned char>> cache;
+		cache.push(make_pair(root, 0));
+		TreeNode *first = nullptr, *second = nullptr, *last = nullptr;
+		while (!(cache.empty() || first && second))
+		{
+			pair<TreeNode *, unsigned char> &t = cache.top();
+			switch (t.second)
+			{
+			case 0:
+				t.second++;
+				if (t.first->left)
+				{
+					cache.push(make_pair(t.first->left, 0));
+					break;
+				}
+			case 1:
+				t.second++;
+				if (!first)
+				{
+					if (last && t.first->val < last->val)
+						first = last;
+				} else
+				{
+					if (t.first->val > first->val)
+					{
+						second = last;
+						break;
+					}
+				}
+				last = t.first;
+				if (t.first->right)
+				{
+					cache.push(make_pair(t.first->right, 0));
+					break;
+				}
+			case 2:
+				cache.pop();
+			}
+		}
+		if (first)
+			swap(first->val, second ? second->val : last->val);
 	}
 };
