6710191f93d1a673b3efbb9a62033e85f983a3b0
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-02-23 17:35:00 +0800

    141.linked-list-cycle

---
+++ 141.linked-list-cycle/solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode *> cache;
+        while (head) {
+            if (!cache.insert(head).second) return true;
+            head = head->next;
+        }
+        return false;
+    }
+};
