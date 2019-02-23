8447ed4f23cc3639002c767240c87d9df6454449
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-02-23 18:45:00 +0800

    145.binary-tree-postorder-traversal

--- 145.binary-tree-postorder-traversal/solution.cpp
+++ 145.binary-tree-postorder-traversal/solution.cpp
@@ -10,15 +10,17 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        _postorderTraversal(root);
-        return result;
+        if (!root) return vector<int>();
+        list<int> resultlist;
+        stack<TreeNode*> cache;
+        cache.push(root);
+        while (!cache.empty()) {
+            TreeNode *p = cache.top();
+            cache.pop();
+            resultlist.push_front(p->val);
+            if (p->left) cache.push(p->left);
+            if (p->right) cache.push(p->right);
+        }
+        return vector<int>(resultlist.begin(), resultlist.end());
     }
-private:
-    void _postorderTraversal(TreeNode* root) {
-        if (!root) return;
-        _postorderTraversal(root->left);
-        _postorderTraversal(root->right);
-        result.push_back(root->val);
-    }
-    vector<int> result;
 };
