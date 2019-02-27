59bc2aa2902b415742efedd2eb5f16d2de79c42c
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-02-27 17:58:00 +0800

    203.remove-linked-list-elements

---
+++ 203.remove-linked-list-elements/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode t1(0);
+        ListNode *p = &t1;
+        t1.next = head;
+        while (p->next) {
+            if (p->next->val == val) {
+                p->next = p->next->next;
+            } else {
+                p = p->next;
+            }
+        }
+        return t1.next;
+    }
+};
