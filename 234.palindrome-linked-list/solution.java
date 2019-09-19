cf41e5ce71d7727084f2a5ba29fd4f2cec787d06
奇葩の灵梦 <cutereimu@vip.qq.com>
2019-09-19 19:07:00 +0800

    234.palindrome-linked-list

---
+++ 234.palindrome-linked-list/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        for (int i = 0; i < list.size() / 2; i++) {
+            if (!list.get(i).equals(list.get(list.size() - 1 - i))) return false;
+        }
+        return true;
+    }
+}
