e99ee1e488ae10e491aef8d05314efcd6e275608
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-07-24 22:54:00 +0800

    5.longest-palindromic-substring

---
+++ 5.longest-palindromic-substring/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ss = "#".join(list(s))
+        max_len = 0
+        val = ""
+        for i in range(len(ss)):
+            j = 1
+            while i - j >= 0 and i + j < len(ss) and ss[i - j] == ss[i + j]:
+                j += 1
+            cur = ss[i - j + 1:i + j].replace("#", "")
+            if max_len < len(cur):
+                max_len = len(cur)
+                val = cur
+        return val.replace("#", "")
