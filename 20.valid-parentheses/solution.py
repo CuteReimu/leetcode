02a653b6ebc265f43d19d9c0e9b0d299a6051847
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-08-14 17:07:00 +0800

    20.valid-parentheses

---
+++ 20.valid-parentheses/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == "}":
+                if len(stack) == 0 or stack[-1] != "{":
+                    return False
+                stack.pop()
+            elif i == "]":
+                if len(stack) == 0 or stack[-1] != "[":
+                    return False
+                stack.pop()
+            elif i == ")":
+                if len(stack) == 0 or stack[-1] != "(":
+                    return False
+                stack.pop()
+            else:
+                stack.append(i)
+        return len(stack) == 0
