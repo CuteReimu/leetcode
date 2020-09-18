39e65ddcf6476c626e609a4fc5bda3f952fd8964
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-09-18 15:43:00 +0800

    47.permutations-ii

--- 47.permutations-ii/solution.cpp
+++ 47.permutations-ii/solution.cpp
@@ -1,44 +1,25 @@
-bool operator<(const vector<int> &v1, const vector<int> &v2) {
-	for (int i = 0;;i++) {
-		if (i == v2.size())
-			return false;
-		if (i == v1.size())
-			return true;
-		if (v1[i] != v2[i])
-			return v1[i] < v2[i];
-	}
-}
-
 class Solution {
 public:
 	vector<vector<int>> permuteUnique(vector<int>& nums) {
 		if (nums.size() == 0)
 			return vector<vector<int>>();
-		set<list<int>> result = permuteUnique(nums, 0);
-		vector<vector<int>> result2;
-		for (auto &l : result) {
-			result2.emplace_back(l.begin(), l.end());
-		}
-		return result2;
-	}
-
-private:
-	set<list<int>> permuteUnique(const vector<int>&nums, int i) {
-		set<list<int>> result;
-		if (nums.size() == i) {
-			result.emplace();
-			return result;
-		}
-		set<list<int>> result2 = permuteUnique(nums, i+1);
-		for (auto val : result2) {
-			for (auto it = val.begin();; it++) {
-				it = val.insert(it, nums[i]);
-				result.insert(val);
-				it = val.erase(it);
-				if (it == val.end())
-					break;
+		vector<vector<int>> v1, v2;
+		vector<vector<int>> *result = &v1, *another = &v2;
+		v1.emplace_back();
+		for (int num : nums) {
+			for (auto it = result->begin(); it != result->end(); it++) {
+				for (int j = 0;; j++) {
+					another->push_back(*it);
+					another->back().insert(another->back().begin() + j, num);
+					if (j >= it->size() || (*it)[j] == num)
+						break;
+				}
 			}
+			result->clear();
+			auto temp = result;
+			result = another;
+			another = temp;
 		}
-		return result;
+		return *result;
 	}
 };
