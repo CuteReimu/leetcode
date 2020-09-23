4e784f434c5e8bd968454b16702fd3f67502a9e7
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-09-23 10:59:00 +0800

    342.power-of-four

--- 342.power-of-four/solution.cpp
+++ 342.power-of-four/solution.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        if (num <= 0) return false;
-        while (num != 1) {
-            if (num % 4) return false;
-            num /= 4;
-        }
-        return true;
+        return
+        num == 0x01 ||
+        num == 0x04 ||
+        num == 0x10 ||
+        num == 0x40 ||
+        num == 0x100 ||
+        num == 0x400 ||
+        num == 0x1000 ||
+        num == 0x4000 ||
+        num == 0x10000 ||
+        num == 0x40000 ||
+        num == 0x100000 ||
+        num == 0x400000 ||
+        num == 0x1000000 ||
+        num == 0x4000000 ||
+        num == 0x10000000 ||
+        num == 0x40000000;
     }
 };
