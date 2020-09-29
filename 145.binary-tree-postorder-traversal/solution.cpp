7663dde2cfdaed4237c59c2100ad5a5e8960da5b
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-09-29 12:27:00 +0800

    145.binary-tree-postorder-traversal

--- 145.binary-tree-postorder-traversal/solution.cpp
+++ 145.binary-tree-postorder-traversal/solution.cpp
@@ -4,23 +4,24 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
+	vector<int> postorderTraversal(TreeNode* root) {
         if (!root) return vector<int>();
-        list<int> resultlist;
-        stack<TreeNode*> cache;
-        cache.push(root);
-        while (!cache.empty()) {
-            TreeNode *p = cache.top();
-            cache.pop();
-            resultlist.push_front(p->val);
-            if (p->left) cache.push(p->left);
-            if (p->right) cache.push(p->right);
-        }
-        return vector<int>(resultlist.begin(), resultlist.end());
-    }
+		stack<TreeNode *> s;
+		s.push(root);
+		vector<int> vec;
+		while (!s.empty()) {
+			auto top = s.top();
+			if (!top->right && !top->left) {vec.push_back(top->val); s.pop();}
+			if (top->right) {s.push(top->right); top->right = nullptr;}
+			if (top->left) {s.push(top->left); top->left = nullptr;}
+		}
+		return vec;
+	}
 };
