abd38f675f83b4ed7fe358e23489fdcd50684160
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-10-13 14:41:00 +0800

    24.swap-nodes-in-pairs

--- 24.swap-nodes-in-pairs/solution.cpp
+++ 24.swap-nodes-in-pairs/solution.cpp
@@ -3,33 +3,20 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-	ListNode* swapPairs(ListNode* head) {
-		if (!head || !head->next)
-		{
-			return head;
-		}
-		ListNode *p = head->next;
-		head->next = head->next->next;
-		p->next = head;
-		head = p;
-		p = p->next;
-		ListNode *pp = p->next;
-		while (pp)
-		{
-			ListNode *q = pp->next;
-			if (!q)
-				break;
-			pp->next = q->next;
-			q->next = pp;
-			p->next = q;
-			p = pp;
-			pp = p->next;
-		}
-		return head;
-	}
+    ListNode* swapPairs(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return head;
+        ListNode* p1 = head;
+        ListNode* p2 = head->next;
+        p1->next = swapPairs(p2->next);
+        p2->next = p1;
+        return p2;
+    }
 };
