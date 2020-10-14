aeb8c73fd8d346892cd57da242444d2640f416d4
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-10-14 15:14:00 +0800

    1002.find-common-characters

--- 1002.find-common-characters/solution.cpp
+++ 1002.find-common-characters/solution.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-	vector<string> commonChars(vector<string>& A) {
-		int result['z' + 1] = {0};
-		for (int i = 0; i < A[0].size(); i++) {
-			result[A[0][i]]++;
-		}
-		for (int i = 1; i < A.size(); i++) {
-			int result2['z' + 1] = {0};
-			for (int j = 0; j < A[i].size(); j++) {
-				result2[A[i][j]]++;
-			}
-			for (char j = 'a'; j <= 'z'; j++) {
-				result[j] = min(result[j], result2[j]);
-			}
-		}
-		vector<string> vec;
-		for (char c = 'a'; c <= 'z'; c++) {
-			for (int i = 0; i < result[c]; i++)
-				vec.emplace_back(1, c);
-		}
-		return vec;
-	}
+    vector<string> commonChars(vector<string>& A) {
+        string &s = A.front();
+        int arr[26] = {0};
+        for (char c : s) {
+            arr[c - 'a']++;
+        }
+        for (int i = 1; i < A.size(); i++) {
+            int arr0[26] = {0};
+            for (char c : A[i]) {
+                arr0[c - 'a']++;
+            }
+            for (int j = 0; j < 26; j++) {
+                arr[j] = min(arr[j], arr0[j]);
+            }
+        }
+        vector<string> result;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                result.emplace_back(1, 'a' + i);
+            }
+        }
+        return result;
+    }
+
+private:
+    void toArr(const string &s, int (&arr)[26]) {
+        for (char c : s) {
+            arr[c - 'a']++;
+        }
+    }
 };
