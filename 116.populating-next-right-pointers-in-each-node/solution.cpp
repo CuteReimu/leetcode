2cbeb229a320ce547d3aba29a99c37e64a931e24
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-10-15 15:19:00 +0800

    116.populating-next-right-pointers-in-each-node

--- 116.populating-next-right-pointers-in-each-node/solution.cpp
+++ 116.populating-next-right-pointers-in-each-node/solution.cpp
@@ -1,24 +1,58 @@
-/**
- * Definition for binary tree with next pointer.
- * struct TreeLinkNode {
- *  int val;
- *  TreeLinkNode *left, *right, *next;
- *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
- * };
- */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    void connect(TreeLinkNode *root) {
-        if (!root) return;
-        connect(root->left);
-        connect(root->right);
-        TreeLinkNode *p = root->left;
-        TreeLinkNode *q = root->right;
+    Node* connect(Node* root) {
+        Node *nextLine = nullptr;
+        Node *p = root;
+        Node *lastNode = nullptr;
         while (p)
         {
-            p->next = q;
-            p = p->right;
-            q = q->left;
+            if (!nextLine) nextLine = p->left;
+            if (!nextLine) nextLine = p->right;
+            if (p->left && p->right)
+            {
+                p->left->next = p->right;
+                if (lastNode) lastNode->next = p->left;
+                lastNode = p->right;
+            }
+            else if (p->left)
+            {
+                if (lastNode) lastNode->next = p->left;
+                lastNode = p->left;
+            }
+            else if (p->right)
+            {
+                if (lastNode) lastNode->next = p->right;
+                lastNode = p->right;
+            }
+            if (p->next)
+            {
+                p = p->next;
+            }
+            else
+            {
+                p = nextLine;
+                nextLine = nullptr;
+                lastNode = nullptr;
+            }
         }
+        return root;
     }
 };
