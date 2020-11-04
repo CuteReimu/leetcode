6bf37e3ed21abbf3b2cde84bf9450540ae84f1b9
奇葩の灵梦 <cutereimu@vip.qq.com>
2020-11-04 18:01:00 +0800

    56.merge-intervals

---
+++ 56.merge-intervals/solution.go
@@ -0,0 +1,33 @@
+func merge(intervals [][]int) [][]int {
+	m := make(map[int]int)
+	for _, interval := range intervals {
+		m[interval[0]]++
+		m[interval[1]]--
+	}
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	temp := 0
+	var last int
+	var result [][]int
+	for _, key := range keys {
+		if m[key] > 0 {
+			if temp == 0 {
+				last = key
+			}
+			temp += m[key]
+		} else if m[key] < 0 {
+			temp += m[key]
+			if temp == 0 {
+				result = append(result, []int{last, key})
+			}
+		} else {
+			if temp == 0 {
+				result = append(result, []int{key, key})
+			}
+		}
+	}
+	return result
+}
