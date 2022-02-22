2789749d0a220d3bc54b0be236b07df135ff0dfc
奇葩の灵梦 <cutereimu@vip.qq.com>
2022-02-22 19:45:00 +0800

    2177.find-three-consecutive-integers-that-sum-to-a-given-number

---
+++ 2177.find-three-consecutive-integers-that-sum-to-a-given-number/solution.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public long[] SumOfThree(long num) {
+        if (num % 3 != 0) {
+            return new long[0];
+        }
+        num /= 3;
+        return new long[]{num-1, num, num+1};
+    }
+}
