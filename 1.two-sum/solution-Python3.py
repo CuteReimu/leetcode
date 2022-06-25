4c27ff63d492bcfa515285fe6bff6ce7a9a5d0a6
奇葩の灵梦 <cutereimu@vip.qq.com>
2022-06-25 18:19:00 +0800

    1.two-sum

--- 1.two-sum/solution-Python3.py
+++ 1.two-sum/solution-Python3.py
@@ -1,14 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        cache = {}
         for i in range(len(nums)):
-            tmp = cache.get(nums[i])
-            if cache.get(nums[i]) == None:
-                tmp = []
-                cache[nums[i]] = tmp
-            tmp.append(i)
-        for i in range(len(nums)):
-            for j in cache.get(target - nums[i], []):
-                if i != j:
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
                     return [i, j]
-        return []
