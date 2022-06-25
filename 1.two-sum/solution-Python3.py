ef7953d3ca2ad9ce62a0b7c1c32d943be1bd67e8
奇葩の灵梦 <cutereimu@vip.qq.com>
2022-06-25 18:24:00 +0800

    1.two-sum

--- 1.two-sum/solution-Python3.py
+++ 1.two-sum/solution-Python3.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            d[nums[i]] = i
+        for i in range(len(nums)):
+            expect_num = target - nums[i]
+            if expect_num in d and i != d[expect_num]:
+                return [i, d[expect_num]]
