dc327bce709bef01a64ea51ec8bbba521cb78d9a
奇葩の灵梦 <cutereimu@vip.qq.com>
2023-04-28 15:20:00 +0800

    27.remove-element

---
+++ 27.remove-element/solution.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var j = 0
+        for (i in nums.indices) {
+            if (nums[i] != `val`)
+                nums[j++] = nums[i]
+        }
+        return j
+    }
+}
