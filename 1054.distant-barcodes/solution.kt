df179c938c14e74e75b58b74f2d934e20c535f6f
奇葩の灵梦 <cutereimu@vip.qq.com>
2023-05-15 00:13:00 +0800

    1054.distant-barcodes

--- 1054.distant-barcodes/solution.kt
+++ 1054.distant-barcodes/solution.kt
@@ -1,48 +1,24 @@
-import java.util.TreeMap
+import java.util.PriorityQueue
 
 class Solution {
     fun rearrangeBarcodes(barcodes: IntArray): IntArray {
         val counts = HashMap<Int, Int>()
         for (i in barcodes)
             counts.compute(i) { _, v -> (v ?: 0) + 1 }
-        val countsVal = TreeMap<Int, ArrayList<Int>>(Collections.reverseOrder())
+        val countsVal = PriorityQueue<Pair<Int, Int>> { o1, o2 -> o2.second.compareTo(o1.second) }
         for ((b, c) in counts)
-            countsVal.getOrPut(c) { ArrayList() }.add(b)
+            countsVal.add(b to c)
         var lastVal = 0
-        val result = IntArray(barcodes.size)
-        for (i in result.indices) {
+        return IntArray(barcodes.size) { _ ->
             val it = countsVal.iterator()
-            var entry = it.next()
-            val v = entry.value.last()
-            var count = entry.key
-            (if (lastVal != v) {
-                entry.value.removeAt(entry.value.size - 1).also { _ ->
-                    if (entry.value.isEmpty())
-                        it.remove()
-                    if (count > 1)
-                        countsVal.getOrPut(count - 1) { ArrayList() }.add(v)
-                }
-            } else {
-                if (entry.value.size >= 2) {
-                    entry.value.removeAt(entry.value.size - 2).also { v ->
-                        if (count > 1)
-                            countsVal.getOrPut(count - 1) { ArrayList() }.add(v)
-                    }
-                } else {
-                    entry = it.next()
-                    count = entry.key
-                    entry.value.removeAt(entry.value.size - 1).also { v ->
-                        if (entry.value.isEmpty())
-                            it.remove()
-                        if (count > 1)
-                            countsVal.getOrPut(count - 1) { ArrayList() }.add(v)
-                    }
-                }
-            }).also { `val` ->
-                lastVal = `val`
-                result[i] = lastVal
-            }
+            var pair = it.next()
+            if (pair.first == lastVal)
+                pair = it.next()
+            it.remove()
+            if (pair.second > 1)
+                countsVal.add(pair.first to pair.second - 1)
+            lastVal = pair.first
+            lastVal
         }
-        return result
     }
 }
