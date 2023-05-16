34ba46dbf0f97f5fa3498590cef60fa2c5a9f4e8
奇葩の灵梦 <cutereimu@vip.qq.com>
2023-05-16 14:47:00 +0800

    1072.flip-columns-for-maximum-number-of-equal-rows

--- 1072.flip-columns-for-maximum-number-of-equal-rows/solution.kt
+++ 1072.flip-columns-for-maximum-number-of-equal-rows/solution.kt
@@ -1,16 +1,35 @@
 class Solution {
     fun maxEqualRowsAfterFlips(matrix: Array<IntArray>): Int {
-        val set = HashMap<String, Int>()
+        val set = HashMap<intArray, Int>()
         for (a in matrix) {
             if (a[0] != 0)
                 for (i in a.indices)
                     a[i] = 1 - a[i]
-            val ch = ByteArray(a.size) { i -> (48 + a[i]).toByte() }
-            set.compute(String(ch)) { _, v -> (v ?: 0) + 1 }
+            set.compute(intArray(a)) { _, v -> (v ?: 0) + 1 }
         }
         var max = 0
         for (count in set.values)
             max = maxOf(max, count)
         return max
+    }
+
+    private class intArray(private val a: IntArray) {
+        private val hash: Int
+
+        init {
+            var hash = 0
+            for (i in a)
+                hash = hash shl 1 or i
+            this.hash = hash
+        }
+
+        override fun hashCode(): Int {
+            return hash
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other !is intArray) return false
+            return other.a.contentEquals(a)
+        }
     }
 }
