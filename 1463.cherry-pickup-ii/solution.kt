e2c7a962352a88c47ca084e6ada97bfa7cee3370
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-05-07 15:26:00 +0800

    1463.cherry-pickup-ii

---
+++ 1463.cherry-pickup-ii/solution.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun cherryPickup(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+        var f = Array(n){IntArray(n){-1}}
+        var g = Array(n){IntArray(n){-1}}
+        f[0][n-1] = grid[0][0] + grid[0][n-1]
+        for (i in 1 until m) {
+            for (j1 in 0 until n) {
+                for (j2 in 0 until n) {
+                    g[j1][j2] = (j1-1..j1+1).maxOf { dj1 ->
+                        (j2-1..j2+1).maxOf { dj2 ->
+                            when {
+                                dj1 !in 0 until n || dj2 !in 0 until n || f[dj1][dj2] == -1 -> -1
+                                j1 == j2 -> f[dj1][dj2] + grid[i][j1]
+                                else -> f[dj1][dj2] + grid[i][j1] + grid[i][j2]
+                            }
+                        }
+                    }
+                }
+            }
+            val temp = f
+            f = g
+            g = temp
+        }
+
+        return (0 until n).maxOf { j1 ->
+            (0 until n).maxOf { j2 -> f[j1][j2] }
+        }
+    }
+}
