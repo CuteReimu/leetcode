24f2fa3189df06cdf77c7d5db0dcf24c08d9918f
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-10-30 15:49:26 +0800

    1.two-sum

--- 1.two-sum/solution.erl
+++ 1.two-sum/solution.erl
@@ -1,18 +1,11 @@
 -spec two_sum(Nums :: [integer()], Target :: integer()) -> [integer()].
 two_sum(Nums, Target) ->
-  A = lists:zip(Nums, lists:seq(0, length(Nums) - 1)),
-  lists:foldl(fun({Num, Idx}, AccIn) ->
-    case AccIn of
-      undefined ->
-        case lists:search(fun({Num2, Idx2}) ->
-          Idx /= Idx2 andalso Target == Num2 + Num
-        end, A) of
-          {value, {Num1, Idx1}} ->
-            [Idx, Idx1];
-          false ->
-            undefined
-        end;
-      Other ->
-        Other
-    end
-  end, undefined, A).
+  f(Nums, 0, Target, #{}).
+
+f([Num | Nums], Idx, Target, Map) ->
+  case Map of
+    #{Target-Num := Idx2} ->
+      [Idx, Idx2];
+    _ ->
+      f(Nums, Idx+1, Target, Map#{Num => Idx})
+  end.
